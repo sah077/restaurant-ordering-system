5db3447f243fb4b7fdf31d137bcbd91d1998a7ed
Sahaj Gyawali <gyawalisahaj12@gmail.com>
2026-07-02 20:26:07 +0545

    added

--- restaurant_system/settings.py
+++ restaurant_system/settings.py
@@ -155,6 +155,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise compressed storage for production performance
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
